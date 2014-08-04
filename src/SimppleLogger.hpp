b52c13869bf8f9832553413f216518af1d23a6a5
Alex Barcelo <abarcelo@ac.upc.edu>
2014-08-04 20:42:16 +0200

    refactoring and completing defines

--- src/SimppleLogger.hpp
+++ src/SimppleLogger.hpp
@@ -9,39 +9,75 @@
 #include <iostream>
 
 /**
-Logs a message to a specified logger with the TRACE level.
-
-@param logger the logger to be used.
-@param message the message string to log.
-*/
-#define STDLOG_TRACE(logger, message) { \
+ * Logs a message to a specified logger with the TRACE level.
+ *
+ * @param logger the logger to be used.
+ * @param message the message string to log.
+ */
+#define SPPLELOG_TRACE(logger, message) { \
         if (logger.getLevel() <= pplelog::TRACE) {\
            std::ostringstream oss_; \
            oss_ << "TRACE " message; \
            logger.log(oss_.str()); }}
 
 /**
-Logs a message to a specified logger with the DEBUG level.
-
-@param logger the logger to be used.
-@param message the message string to log.
-*/
-#define STDLOG_DEBUG(logger, message) { \
+ * Logs a message to a specified logger with the DEBUG level.
+ *
+ * @param logger the logger to be used.
+ * @param message the message string to log.
+ */
+#define SPPLELOG_DEBUG(logger, message) { \
         if (logger.getLevel() <= pplelog::DEBUG) {\
            std::ostringstream oss_; \
            oss_ << "DEBUG " << message; \
            logger.log(oss_.str()); }}
 
 /**
-Logs a message to a specified logger with the TRACE level.
-
-@param logger the logger to be used.
-@param message the message string to log.
-*/
-#define STDLOG_INFO(logger, message) { \
+ * Logs a message to a specified logger with the INFO level.
+ *
+ * @param logger the logger to be used.
+ * @param message the message string to log.
+ */
+#define SPPLELOG_INFO(logger, message) { \
         if (logger.getLevel() <= pplelog::INFO) {\
            std::ostringstream oss_; \
            oss_ << "INFO " message; \
+           logger.log(oss_.str()); }}
+
+/**
+ * Logs a message to a specified logger with the WARN level.
+ *
+ * @param logger the logger to be used.
+ * @param message the message string to log.
+ */
+#define SPPLELOG_WARN(logger, message) { \
+        if (logger.getLevel() <= pplelog::WARN) {\
+           std::ostringstream oss_; \
+           oss_ << "WARN " message; \
+           logger.log(oss_.str()); }}
+
+/**
+ * Logs a message to a specified logger with the ERROR level.
+ *
+ * @param logger the logger to be used.
+ * @param message the message string to log.
+ */
+#define SPPLELOG_ERROR(logger, message) { \
+        if (logger.getLevel() <= pplelog::ERROR) {\
+           std::ostringstream oss_; \
+           oss_ << "ERROR " message; \
+           logger.log(oss_.str()); }}
+
+/**
+ * Logs a message to a specified logger with the FATAL level.
+ *
+ * @param logger the logger to be used.
+ * @param message the message string to log.
+ */
+#define SPPLELOG_FATAL(logger, message) { \
+        if (logger.getLevel() <= pplelog::FATAL) {\
+           std::ostringstream oss_; \
+           oss_ << "FATAL " message; \
            logger.log(oss_.str()); }}
 
 
@@ -51,14 +87,14 @@ namespace pplelog {
  * List of possible levels associated to a logger
  */
 enum levels{
-	ALWAYS_LOG = -5,
-	TRACE,
-	DEBUG = 0,
-	INFO,
-	WARN,
-	ERROR,
-	FATAL,
-	NEVER_LOG = 1000
+    ALWAYS_LOG = -5,
+    TRACE,
+    DEBUG = 0,
+    INFO,
+    WARN,
+    ERROR,
+    FATAL,
+    NEVER_LOG = 1000
 };
 
 class LevelTracker;
@@ -75,44 +111,44 @@ SimppleLogger &getLogger(const std::string &s, const std::string &parent);
 
 class pplelog::LevelTracker {
 public:
-	LevelTracker();
-	void setParent(LevelTracker &parent);
-	operator levels();
-	void setLevel(levels &changeLevel);
+    LevelTracker();
+    void setParent(LevelTracker &parent);
+    operator levels();
+    void setLevel(levels &changeLevel);
 private:
-	void deepLevelChanger(levels *precursor, levels *future);
-	levels localLevel;
-	LevelTracker *parent;
-	levels *assignedLevel;
-	std::vector<LevelTracker *> children;
+    void deepLevelChanger(levels *precursor, levels *future);
+    levels localLevel;
+    LevelTracker *parent;
+    levels *assignedLevel;
+    std::vector<LevelTracker *> children;
 };
 
 class pplelog::SimppleLogger {
 public:
-	levels getLevel() const;
-	void setLogLevel(levels level);
-	void log(std::string s_);
-	const std::string &getName() const;
+    levels getLevel() const;
+    void setLogLevel(levels level);
+    void log(std::string s_);
+    const std::string &getName() const;
 
-	void setParent(const std::string &parent);
-	void setParent(const SimppleLogger &parent);
+    void setParent(const std::string &parent);
+    void setParent(const SimppleLogger &parent);
 
 
 private:
-	std::string name;
-	size_t hash;
-	SimppleLogger(const std::string &s);
-	~SimppleLogger();
+    std::string name;
+    size_t hash;
+    SimppleLogger(const std::string &s);
+    ~SimppleLogger();
 
-	static void setParent(size_t hash_base, size_t hash_parent);
+    static void setParent(size_t hash_base, size_t hash_parent);
 
 // Friends
-	friend levels getDefaultLevel();
-	friend void setDefaultLevel(levels newlevel);
-	friend void setLogLevel(const std::string name, levels level);
-	friend SimppleLogger &getLogger(const std::string &s);
-	friend SimppleLogger &getLogger(const std::string &s, const std::string &parent);
-	friend void setParent(const std::string &base, const std::string &parent);
+    friend levels getDefaultLevel();
+    friend void setDefaultLevel(levels newlevel);
+    friend void setLogLevel(const std::string name, levels level);
+    friend SimppleLogger &getLogger(const std::string &s);
+    friend SimppleLogger &getLogger(const std::string &s, const std::string &parent);
+    friend void setParent(const std::string &base, const std::string &parent);
 };
 
 #endif /* SIMPPLELOGGER_HPP_ */
